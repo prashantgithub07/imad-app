fb357def8905e87242198c2f20bd03d33852d66f
prashantgithub07 <31267091+prashantgithub07@users.noreply.github.com>
2017-08-23 19:57:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,11 @@
 console.log('Loaded!');
-var element = document.getElementById('main-text');
 
-element.innerHTML = 'New value'
+//change the text of main-text div
+var element = document.getElementById('main-text');
+element.innerHTML = 'New value';
+
+//maove the image
+var img = document.getElementById('madi');
+img.onclick = function () {
+  img.style.marginLeft = '100px';  
+};
