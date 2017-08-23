9d2630d78b8cbc665cc67bfbf65e4cc493183042
prashantgithub07 <31267091+prashantgithub07@users.noreply.github.com>
2017-08-23 19:13:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne = {
+var articles {
+  articleOne = {
     title: 'Article one | Prashant Bhole',
     heading: 'Article One',
     date: 'Aug 21, 2017',
@@ -17,6 +18,29 @@ var articleOne = {
             <p>
                 This is th econtents of my first article. This is th econtents of my first article. This is th econtents of my first articlec. This is th econtents of my first article. This is th econtents of my first article. This is th econtents of my first article. This is th econtents of my first article.
             </p>`
+},
+  articleTwo = {title: 'Article Two | Prashant Bhole',
+    heading: 'Article Two',
+    date: 'Aug 22, 2017',
+    content: `<p> This is th econtents of my second article. This is th econtents of my first article. This is th econtents of my first articlec. This is th econtents of my first article. This is th econtents of my first article. This is th econtents of my first article. This is th econtents of my first article.
+            </p>
+            <p>
+                This is th econtents of my first article. This is th econtents of my first article. This is th econtents of my first articlec. This is th econtents of my first article. This is th econtents of my first article. This is th econtents of my first article. This is th econtents of my first article.
+            </p>
+            <p>
+                This is th econtents of my first article. This is th econtents of my first article. This is th econtents of my first articlec. This is th econtents of my first article. This is th econtents of my first article. This is th econtents of my first article. This is th econtents of my first article.
+            </p>`},
+  articleThree = {title: 'Article Three | Prashant Bhole',
+    heading: 'Article Three',
+    date: 'Aug 23, 2017',
+    content: `<p> This is th econtents of my third article. This is th econtents of my first article. This is th econtents of my first articlec. This is th econtents of my first article. This is th econtents of my first article. This is th econtents of my first article. This is th econtents of my first article.
+            </p>
+            <p>
+                This is th econtents of my first article. This is th econtents of my first article. This is th econtents of my first articlec. This is th econtents of my first article. This is th econtents of my first article. This is th econtents of my first article. This is th econtents of my first article.
+            </p>
+            <p>
+                This is th econtents of my first article. This is th econtents of my first article. This is th econtents of my first articlec. This is th econtents of my first article. This is th econtents of my first article. This is th econtents of my first article. This is th econtents of my first article.
+            </p>`}
 };
 
 function createTemplate (data) {
