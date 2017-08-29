55f959ef7f3910b701e78e7e1f8383492cad8937
prashantgithub07 <31267091+prashantgithub07@users.noreply.github.com>
2017-08-29 08:35:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ app.get('/submit-name', function (req, res) { // URL : /submit-name?name=xxxx
 
 app.get('/articles/:articleName', function (req, res) {
     //selelct * from article where title= 'article-one'
-    pool.query("SELECT * FROM article WHERE title= '" + req.params.articleName,"'" function(err, result) {
+    pool.query("SELECT * FROM article WHERE title= " + req.params.articleName, function(err, result) {
         if(err) {
           res.status(500).send(err.toString());  
         } else {
