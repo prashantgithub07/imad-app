05641e313141f61d49f44b66c167d41254d43a18
prashantgithub07 <31267091+prashantgithub07@users.noreply.github.com>
2017-08-29 08:40:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -129,7 +129,7 @@ app.get('/articles/:articleName', function (req, res) {
             if (result.rows.length === 0) {
                 res.status(404).send('Article not found');
             } else {
-                var articleName = result.rows[0];
+                var articleData = result.rows[0];
                 res.send(createTemplate(articleData));
             }
         }
