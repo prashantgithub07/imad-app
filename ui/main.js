b4d8f6a809a331348a0ed48915f1611c9c263a4e
prashantgithub07 <31267091+prashantgithub07@users.noreply.github.com>
2017-08-30 19:45:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,32 +1,7 @@
 // Counter Code
-var button = document.getElementById('counter'); 
-button.onclick = function () {
-    
-    //create request object
-    var request = new XMLHttpRequest();
-    
-    
-    //cpture the response and store it in variable
-    request.onreadystatechange = function () {
-      if (request.readyState === XMLHttpRequest.DONE) {
-          //Take some action
-          if (request.status === 200) {
-              var counter = request.responseText;
-              var span = document.getElementById('count');
-              span.innerHTML = counter.toString();
-          }
-      } 
-      //not done yet
-        
-    };
-    
-    //Make the request 
-    request.open('GET', 'http://pdbbhole.imad.hasura-app.io/counter', true);
-    request.send(null);
-     
-};
 
-//submit name
+
+//submit username/password to login
 
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
@@ -41,14 +16,12 @@ submit.onclick = function () {
       if (request.readyState === XMLHttpRequest.DONE) {
           //Take some action
           if (request.status === 200) {
-              var names = request.responseText;
-              names = JSON.parse(names);
-               var list = '';
-                for (var i=0; i<names.length; i++) {
-                list += '<li>' + names[i] + '</li>';
-            }
-            var ul = document.getElementById('namelist');
-            ul.innerHTML = list;
+              console.log('user logged in');
+              alert('Login Sucessfully');
+          } else if (request.status === 403) {
+              alert('Username/Password is incorrect');
+          } else if (request.status === 500) {
+              alert('Something went Wrong on the server');
           }
       } 
       //not done yet
@@ -56,10 +29,13 @@ submit.onclick = function () {
     };
     
     //Make the request 
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    request.open('GET', 'http://pdbbhole.imad.hasura-app.io/submit-name?name=' + name, true);
-    request.send(null);
+    var username  = document.getElementById('username').value;
+    var password  = document.getElementById('password').value;
+    console.log(username);
+    console.log(password); 
+    request.open('POST', 'http://pdbbhole.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
      
    
 };
