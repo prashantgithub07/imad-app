da026ba8f2cb3da141800532c674d7730fe2bb55
prashantgithub07 <31267091+prashantgithub07@users.noreply.github.com>
2017-09-02 13:30:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@
 
 //submit username/password to login
 
-var submit = document.getElementById('submit_btn');
+var submit = document.getElementById('login_btn');
 submit.onclick = function () {
     // should make request to server and send name
     // Capture a list of names and render it as a list
@@ -38,4 +38,32 @@ submit.onclick = function () {
     request.send(JSON.stringify({username: username, password: password}));
      
    
+};
+
+var register = document.getElementById('register_btn');
+register.onclick = function () {
+    // Create a request object
+    var request = new XMLHttpRequest();
+    
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          // Take some action
+          if (request.status === 200) {
+              alert('User created successfully');
+          } else if (request.status === 500) {
+              alert('Could not register the user');
+          }
+      }  
+      // Not done yet
+    };
+    
+    // Make the request
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST', 'http://coco98.imad.hasura-app.io/create-user', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));        
 };
