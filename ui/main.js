7cfbe63e4577c5228c68af5641570d2ce4c0d831
prashantgithub07 <31267091+prashantgithub07@users.noreply.github.com>
2017-09-02 14:44:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,6 @@ function loadLoginForm () {
 var loginHtml = `
 <h3>Login/Register to unlock awesome features</h3>
     <input type="text" id="username" placeholder="username" />
-    var request = new XMLHttpRequest();
      <input type="password" id="password" />
         <br/><br/>
         <input type="submit" id="login_btn" value="Login" />
