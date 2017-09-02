e84995d8eb0d44dda7b496580f6d35d70b3a6c71
prashantgithub07 <31267091+prashantgithub07@users.noreply.github.com>
2017-09-02 15:40:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -220,8 +220,8 @@ app.get('/articles/:articleName', function (req, res) {
 		
 		
 		
-app.get('/ui/style.css', function (req, res) {		
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));		
+app.get('/ui/:fileName', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', req.params.fileName));		
 });		
 		
 app.get('/ui/main.js', function (req, res) {		
