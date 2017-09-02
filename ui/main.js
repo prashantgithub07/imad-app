8a58af65c938fa127060adf91b702ca38be7a3e2
prashantgithub07 <31267091+prashantgithub07@users.noreply.github.com>
2017-09-02 15:07:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -117,12 +117,12 @@ function loadLoggedInUser (username) {
                 var content = '<ul>';
                 var articleData = JSON.parse(this.responseText);
                 for (var i=0; i< articleData.length; i++) {
-                    content += `<li><a href="/articles/${articleData[i].title}">${articleData[i].heading}</a></li>`;
+                    content += `<li><a href="/articles/${articleData[i].title}">${articleData[i].heading}</a> (${articleData[i].date}) </li>`;
                 }
-                content += "</ul>"
+                content += "</ul>";
                 articles.innerHTML = content;
             } else {
-                articles.innerHTML('Oops! Could not load all articles!')
+                articles.innerHTML('Oops! Could not load all articles!');
             }
         }
     };
