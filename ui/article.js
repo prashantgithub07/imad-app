9775b6b4406627acf8b4c90b30c2779d75d34ab1
prashantgithub07 <31267091+prashantgithub07@users.noreply.github.com>
2017-09-03 12:41:54 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -5,6 +5,7 @@
       var commentFormHtml = `
           <h5>Submit a comment</h5>
           <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
+          - </textarea>
            <br/>
          <input type="submit" id="submit" value="Submit" />
           <br/>
